d259db5689d71424d3e76105e0562a902e890a0a
Se0jwon <sjso11426682@gmail.com>
2025-09-02 17:46:30 +0900

    2025-09-02 주문과 할인 도메인 실행 테스트

---
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -0,0 +1,31 @@
+package hello.core.order;
+
+import hello.core.discount.FixDiscountPolicy;
+import hello.core.discount.DiscountPolicy;
+import hello.core.discount.RateDiscountPolicy;
+import hello.core.member.Member;
+import hello.core.member.MemberRepository;
+import hello.core.member.MemoryMemberRepository;
+
+public class OrderServiceImpl implements OrderService {
+
+
+    private final MemberRepository memberRepository;
+    private final DiscountPolicy discountPolicy;
+
+    public OrderServiceImpl(MemoryMemberRepository memoryMemberRepository, FixDiscountPolicy fixDiscountPolicy) {
+        this.memberRepository = memoryMemberRepository;
+        this.discountPolicy = fixDiscountPolicy;
+    }
+//    private final DiscountPolicy discountPolicy = new RateDiscountPolicy();
+
+
+    @Override
+    public Order createOrder(Long memberId, String itemName, int itemPrice) {
+
+        Member member = memberRepository.findById(memberId);
+        int discoutPrice = discountPolicy.discount(member, itemPrice);
+
+        return new Order(memberId, itemName, itemPrice, discoutPrice);
+    }
+}
